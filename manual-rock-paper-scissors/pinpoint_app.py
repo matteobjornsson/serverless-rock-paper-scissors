3eec415a55e75763b04d653d3d3b069b1fabe554
matteobjornsson <matteobjornsson@gmail.com>
2021-04-17 14:48:01 -0600

    add pinpoint app creation

---
+++ manual-rock-paper-scissors/pinpoint_app.py
@@ -0,0 +1,44 @@
+import boto3
+import pprint
+from botocore.exceptions import ClientError
+import logging
+logging.basicConfig(filename='pinpoint.log', level=logging.DEBUG)
+
+region = "us-east-1"
+
+client = boto3.client('pinpoint',region_name=region)
+
+# create a pinpoint app 'rock_paper_scissors'
+try:                                      
+    response = client.create_app(
+        CreateApplicationRequest={
+            'Name': 'rock_paper_scissors',
+            'tags':{}
+        }
+    )
+except ClientError as e:
+    logging.error(e.response['Error']['Message'])
+else:
+    logging.debug("SMS channel enabled.")
+    print("Pinpoint App Created ")
+
+# grab the application ID from the 
+applicationID = response['ApplicationResponse']['Id']
+
+# enable SMS channel on Pinpoint app
+try:
+    response = client.update_sms_channel(
+        ApplicationId=applicationID,
+        SMSChannelRequest={
+            'Enabled': True
+            # 'SenderId': 'string',
+            # 'ShortCode': 'string'
+        }
+    )
+except ClientError as e:
+    logging.error(e.response['Error']['Message'])
+else:
+    logging.debug("SMS channel enabled.")
+    print("SMS channel enabled ")
+
+
