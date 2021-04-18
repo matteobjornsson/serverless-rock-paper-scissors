5de864e9cfbf8455defec13fbf2254ce5bfe4983
matteobjornsson <matteobjornsson@gmail.com>
2021-04-18 17:17:45 -0600

    add delete method

--- Pinpoint.py
+++ Pinpoint.py
@@ -5,12 +5,12 @@ logging.basicConfig(filename='rps.log', level=logging.INFO)
 
 region = "us-east-1"
 
-client = boto3.client('pinpoint',region_name=region)
+pinpoint_client = boto3.client('pinpoint',region_name=region)
 
-def create_pinpoint(app_name: str) -> str:
+def create_pinpoint_app(app_name: str) -> str:
     # create a pinpoint app 'rock_paper_scissors'
     try:                                      
-        response = client.create_app(
+        response = pinpoint_client.create_app(
             CreateApplicationRequest={
                 'Name': app_name,
                 'tags':{}
@@ -26,10 +26,25 @@ def create_pinpoint(app_name: str) -> str:
         # return the application ID for further modification.    
         return applicationID        
 
-def enable_pinpoint_SMS(applicationID: str) -> None:
+
+def delete_pinpoint_app(application_id: str) -> dict:
+    # Delete a pinpoint app
+    try:                                      
+        response = pinpoint_client.delete_app(
+            ApplicationId=application_id
+        )
+    except ClientError as e:
+        logging.error(e.response['Error']['Message'])
+    else:
+        success_msg = "Pinpoint App Deleted."
+        logging.info(success_msg)
+        return response    
+
+
+def enable_pinpoint_SMS(applicationID: str) -> dict:
     # enable SMS channel on Pinpoint app
     try:
-        _ = client.update_sms_channel(
+        response = pinpoint_client.update_sms_channel(
             ApplicationId=applicationID,
             SMSChannelRequest={
                 'Enabled': True
@@ -43,5 +58,5 @@ def enable_pinpoint_SMS(applicationID: str) -> None:
         success_msg = "SMS Channel Enabled."
         logging.info(success_msg)
         print(success_msg)
-
+        return response
 
