a1a4a807671626230b230b840a7ceb5da0b62585
matteobjornsson <matteobjornsson@gmail.com>
2021-04-19 23:08:22 -0600

    add a 'send_sms' function to Pinpoint

--- services/Pinpoint.py
+++ services/Pinpoint.py
@@ -1,6 +1,7 @@
 import boto3
 from botocore.exceptions import ClientError
 import logging
+import pprint
 
 logging.basicConfig(filename="rps.log", level=logging.INFO)
 
@@ -53,10 +54,39 @@ def enable_pinpoint_SMS(applicationID: str) -> dict:
         logging.info("Pinpoint SMS Enabled.")
         return response
 
+def send_SMS_message(phone_number: str, message: str, pinpoint_app_id: str) -> None:
+    # Create a new client and specify a region.
+    client = boto3.client("pinpoint")
+    try:
+        response = client.send_messages(
+            ApplicationId=pinpoint_app_id,
+            MessageRequest={
+                "Addresses": {phone_number: {"ChannelType": "SMS"}},
+                "MessageConfiguration": {
+                    "SMSMessage": {
+                        "Body": message,
+                        "MessageType": "TRANSACTIONAL"
+                    }
+                },
+            },
+        )
+    except ClientError as e:
+        logging.error(e.response["Error"]["Message"])
+    else:
+        result = response['MessageResponse']['Result'][phone_number]
+        pprint.pprint(result)
+        if result['DeliveryStatus'] == 'PERMANENT_FAILURE':
+            logging.error("Message not delivered: %s", result)
+        elif result['DeliveryStatus'] == 'SUCCESSFUL':
+            logging.info("Message sent!")
+        else:
+            logging.warn("Unknown delivery status of SMS message")
 
 if __name__ == "__main__":
+    # test create, enable, and text a number
     app_name = "pinpoint_test_name"
     response = create_pinpoint_app(app_name)
     pinpoint_app_id = response["ApplicationResponse"]["Id"]
-    response = create_pinpoint_app(app_name)
+    enable_pinpoint_SMS(pinpoint_app_id)
+    send_SMS_message("+18001234567", "initial message", pinpoint_app_id)
     delete_pinpoint_app(pinpoint_app_id)
