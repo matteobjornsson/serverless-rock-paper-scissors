7171d27016308ac324ce7fdfddc81ffba1b60c4a
matteobjornsson <matteobjornsson@gmail.com>
2021-04-19 23:09:49 -0600

    add delete lines function to util for teardown

--- util.py
+++ util.py
@@ -31,5 +31,14 @@ def get_keyword_index(string_list: list, keyword: str) -> int:
         if keyword in s:
             return i
     return -1
+
+def delete_lines(file_path: str, line_list: list)-> None:
+    with open(file_path, 'r+') as filehandler:
+        file_lines = filehandler.readlines()
+        filehandler.seek(0)
+        for line in file_lines:
+            if line not in line_list:
+                filehandler.write(line)
+        filehandler.truncate()
         
 
