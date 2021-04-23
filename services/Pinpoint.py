7135de8d0edd99001a8b6bc40e631c54dd81b39b
matteobjornsson <matteobjornsson@gmail.com>
2021-04-22 23:35:29 -0600

    Update Pinpoint comments

--- services/Pinpoint.py
+++ services/Pinpoint.py
@@ -5,17 +5,17 @@
 import boto3
 from botocore.exceptions import ClientError
 import logging
-import pprint
 
 logging.basicConfig(filename="rps.log", level=logging.INFO)
-
 
 pinpoint_client = boto3.client("pinpoint")
 
 
 def create_pinpoint_app(app_name: str) -> dict:
     """
-    TODO: write function description
+    Create a pinpoint app with the given name. 
+    (pinpoint apps are uniquely defined by ID, not name. 
+    One might create many of the same name)
     """
     try:
         response = pinpoint_client.create_app(
@@ -31,7 +31,7 @@ def create_pinpoint_app(app_name: str) -> dict:
 
 def delete_pinpoint_app(application_id: str) -> dict:
     """
-    TODO: write function description
+    Delete pinpoint app by application ID (pinpoint apps are uniquely defined by ID, not name)
     """
     try:
         response = pinpoint_client.delete_app(ApplicationId=application_id)
@@ -45,7 +45,7 @@ def delete_pinpoint_app(application_id: str) -> dict:
 
 def enable_pinpoint_SMS(applicationID: str) -> dict:
     """
-    TODO: write function description
+    Enable SMS channel on the given pinpoint app via ID
     """
     try:
         response = pinpoint_client.update_sms_channel(
@@ -60,10 +60,14 @@ def enable_pinpoint_SMS(applicationID: str) -> dict:
 
 
 def send_SMS_message(phone_number: str, message: str, pinpoint_app_id: str) -> None:
-    # Create a new client and specify a region.
-    client = boto3.client("pinpoint")
+    """
+    Send an sms message to the given phone number using the given pinpoint app.
+    :param phone_number: destination phone number
+    :param message: message to send
+    :param pinpoint_app_id: the id of the pinpoint app used to send the SMS
+    """
     try:
-        response = client.send_messages(
+        response = pinpoint_client.send_messages(
             ApplicationId=pinpoint_app_id,
             MessageRequest={
                 "Addresses": {phone_number: {"ChannelType": "SMS"}},
